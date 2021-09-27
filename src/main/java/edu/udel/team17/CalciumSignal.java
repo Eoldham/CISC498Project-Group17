74f94396f7fde4f2ce156fe6ed0ca244f2974d67
Emma Adelmann <eadel@udel.edu>
2021-09-27 15:55:00 -0400

    starter code

---
+++ src/main/java/edu/udel/team17/CalciumSignal.java
@@ -0,0 +1,21 @@
+package edu.udel.team17;
+
+import ij.IJ;
+import ij.ImageJ;
+import ij.ImagePlus;
+import ij.gui.GenericDialog;
+import ij.plugin.PlugIn;
+import ij.process.ImageProcessor;
+
+public class CalciumSignal implements PlugIn {
+
+    public void run(String arg) {
+        // plugin logic here
+        // https://imagej.net/develop/ij1-plugins
+    }
+
+    public static void main(String[] args) {
+        // make sure this runs in your IDE
+        System.out.println("Hello, world!");
+    }
+}
