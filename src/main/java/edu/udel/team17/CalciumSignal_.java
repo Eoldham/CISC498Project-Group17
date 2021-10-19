15b29975eff1aca723784ec845dde7969fa9cf0d
Emma Adelmann <eadel@udel.edu>
2021-10-19 14:16:29 -0400

    attempt at running plugin

--- src/main/java/edu/udel/team17/CalciumSignal_.java
+++ src/main/java/edu/udel/team17/CalciumSignal_.java
@@ -6,12 +6,16 @@ import ij.ImagePlus;
 import ij.gui.GenericDialog;
 import ij.plugin.PlugIn;
 import ij.process.ImageProcessor;
+import imageJ.plugins.*;
 
-public class CalciumSignal implements PlugIn {
+public class CalciumSignal_ implements PlugIn {
 
     public void run(String arg) {
         // plugin logic here
         // https://imagej.net/develop/ij1-plugins
+
+        PoorMan3DReg_ reg = new PoorMan3DReg_();
+        reg.run(arg);
     }
 
     public static void main(String[] args) {
