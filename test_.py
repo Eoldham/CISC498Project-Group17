7e1409c0f50937ae670b1beec1f5c9a6051f598e
Emma Adelmann <eadel@udel.edu>
2021-11-19 14:29:13 -0500

    trying python stuff

--- test_.py
+++ test_.py
@@ -1,3 +1,4 @@
+"""
 import tkinter as tk
 from tkinter import filedialog
 
@@ -5,5 +6,18 @@ root = tk.Tk()
 root.withdraw()
 
 file_path = filedialog.askopenfilename()
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-1, 1, 50)
+print(x)
+y = 2*x + 1
+
+plt.plot(x, y)
+plt.show()
+
+
 
 # print("Hello, world!")
