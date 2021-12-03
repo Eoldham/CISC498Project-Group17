9d6f7302d081ef836d2f5501aa40f8b369219e96
Emma Adelmann <eadel@udel.edu>
2021-12-03 15:19:50 -0500

    getting an install script started...todo: move jar file to correct location + test

---
+++ USER_INSTALL.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import subprocess
+
+
+# Assuming we are in user's Fiji.app directory
+print("Creating plugins/CalciumSignal directory...\n")
+base = "plugins/CalciumSignal"
+os.mkdir(base)
+print("Done.\n")
+
+# Directories needed by peak finding script
+print("Setting up Python environment...\n")
+pythonscript = os.path.join(base, "pythonscript")
+os.mkdir(pythonscript)
+celldata = os.path.join(pythonscript, "cell_data")
+os.mkdir(celldata)
+
+os.rename("peakscript.py", os.path.join(pythonscript, "peakscript.py"))
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r' 'requirments.txt'])
+
+print("Done.\n")
+
+# Directories needed by ROI Manager
+print("Setting up additional directories...\n")
+edgedata = os.path.join(base, "edge_data")
+os.mkdir(edgedata)
+print("Done.\n")
+
