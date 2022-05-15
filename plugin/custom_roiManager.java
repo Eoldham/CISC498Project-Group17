594da703b8d5a907b7381aca34a187afe902414d
Emily Oldham <Owlanne@udel.edu>
2022-05-15 16:21:42 -0400

    Fixed realResults path, moved peak analysis occur after multi measure is pressed

--- plugin/custom_roiManager.java
+++ plugin/custom_roiManager.java
@@ -12,7 +12,10 @@ import javax.swing.text.NumberFormatter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -22,18 +25,18 @@ import static com.sun.java.accessibility.util.AWTEventMonitor.addKeyListener;
 
 public class custom_roiManager extends PlugInFrame implements ActionListener {
 
+    private final String PYTHONSCRIPT_PATH = "plugins/CalciumSignal/pythonscript";
+
     Panel panel;
     RoiManager rm;
     int cellMin;
     int cellMax;
-    private final String PYTHONSCRIPT_PATH;
     JFormattedTextField minField;
     JFormattedTextField maxField;
     Set<Roi> allRois = new HashSet<Roi>();
 
-    custom_roiManager(String scriptPath){
+    custom_roiManager(){
         super("Custom RoiManager");
-        PYTHONSCRIPT_PATH = scriptPath;
         rm = new RoiManager();
         cellMin = 20;
         cellMax = 200;
@@ -129,7 +132,7 @@ public class custom_roiManager extends PlugInFrame implements ActionListener {
             rm.runCommand("multi-measure");
 
             //Gets active table and saves
-            String path = PYTHONSCRIPT_PATH + "/cell_data/realResults.csv";
+            String path =  "plugins/CalciumSignal/pythonscript/cell_data/realResults.csv";
             ResultsTable results = ij.measure.ResultsTable.getResultsTable();
             try {
                 results.saveAs(path);
@@ -139,12 +142,77 @@ public class custom_roiManager extends PlugInFrame implements ActionListener {
 
             rm.close();
             setVisible(false);
+            peakFinding();
         }
     }
 
     public void addRoi(Roi roi){
         rm.addRoi(roi);
         rm.runCommand("Show All");
+    }
+
+    public void peakFinding(){
+        /*
+        -- PEAK FINDING --
+         */
+        try {
+            // Attempt to find the preferred command or path for python 3
+            String systemPath = System.getenv("PATH");
+            String[] pathLines = systemPath.split(":");
+            String exePath = "python";
+            String os = System.getProperty("os.name");
+
+            if (os.contains("Windows")) {
+                for (String entry : pathLines) {
+                    boolean pyPath = entry.contains("python") || entry.contains("python3");
+
+                    if (pyPath) {
+                        if (entry.contains("python3")) {
+                            exePath = entry.substring(entry.indexOf(System.getProperty("file.separator")) + 1);
+                        }
+                    }
+                }
+            } else {
+                String[] bin = new File("/usr/bin").list();
+                for (String cmdName : bin) {
+                    if (cmdName.equals("python") || cmdName.equals("python3")) {
+                        exePath = cmdName;
+                    }
+                }
+                String[] local = new File("/usr/local/bin").list();
+                for (String cmdName : local) {
+                    if (cmdName.equals("python") || cmdName.equals("python3")) {
+                        // Prefer local distribution (/usr/local/bin)...use full path
+                        exePath = "/usr/local/bin/" + cmdName;
+                    }
+                }
+            }
+
+            // RELATIVE TO LOCATION OF FIJI EXECUTABLE
+            ProcessBuilder processBuilder = new ProcessBuilder(exePath, PYTHONSCRIPT_PATH + "/peakscript.py");
+            processBuilder.redirectErrorStream(true);
+
+            Process process = processBuilder.start();
+            BufferedReader errout = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            String line;
+
+            while ((line = errout.readLine()) != null) {
+                IJ.log(line);
+            }
+
+            // Use for debugging only
+            /*
+            BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line2;
+
+            while ((line2 = input.readLine()) != null) {
+                IJ.log(line2);
+            }
+             */
+
+        } catch (Exception ex) {
+            IJ.log(ex.getMessage());
+        }
     }
 
 }
